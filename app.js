86cd2c35381fbfbeca66a7af26549884bac9dc0d
Glynn Bird <glynn.bird@gmail.com>
2016-01-20 09:37:12 +0000

    tidy up

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 var u = require('url'),
   request = require('request'),
+  _ = require('underscore'),
   debug = require('debug')('cloudantlite'),
+  log = debug,
   errs = require('errs'),
   httpAgent = request;
   configdefaults = { requst: request, url: "http://localhost:5984", requestDefaults: { jar: false}};
@@ -10,195 +12,190 @@ module.exports = function(config) {
 
   // config
   if (typeof config === 'string') {
-    config = { url: config};
+    config = { url: config };
   }
-  var cfg = config ||  configdefaults;
- 
-  // extend dest object by adding things from source
-  var munge = function(dest, source) {
-    if (typeof source == "object") {
-      for (var i in source) {
-        dest[i] = source[i];
+  var cfg = config || configdefaults; 
+  
+  function relax(opts, callback) {
+    if (typeof opts === 'function') {
+      callback = opts;
+      opts = {path: ''};
+    }
+
+    if (typeof opts === 'string') {
+      opts = {path: opts};
+    }
+
+    if (!opts) {
+      opts = {path: ''};
+      callback = null;
+    }
+
+    var qs = _.extend({}, opts.qs);
+
+    var headers = {
+      'content-type': 'application/json',
+      accept: 'application/json'
+    };
+
+    var req = {
+      method: (opts.method || 'GET'),
+      headers: headers,
+      uri: cfg.url
+    };
+
+    var parsed;
+    var rh;
+
+    // https://github.com/mikeal/request#requestjar
+    var isJar = opts.jar || cfg.jar;
+
+    if (isJar) {
+      req.jar = isJar;
+    }
+
+    // http://wiki.apache.org/couchdb/HTTP_database_API#Naming_and_Addressing
+    if (opts.db) {
+      req.uri = urlResolveFix(req.uri, encodeURIComponent(opts.db));
+    }
+
+    if (opts.multipart) {
+      req.multipart = opts.multipart;
+    }
+
+    req.headers = _.extend(req.headers, opts.headers, cfg.defaultHeaders);
+
+    if (opts.path) {
+      req.uri += '/' + opts.path;
+    } else if (opts.doc) {
+      if (!/^_design/.test(opts.doc)) {
+        // http://wiki.apache.org/couchdb/HTTP_Document_API#Naming.2FAddressing
+        req.uri += '/' + encodeURIComponent(opts.doc);
+      } else {
+        // http://wiki.apache.org/couchdb/HTTP_Document_API#Document_IDs
+        req.uri += '/' + opts.doc;
+      }
+
+      // http://wiki.apache.org/couchdb/HTTP_Document_API#Attachments
+      if (opts.att) {
+        req.uri += '/' + opts.att;
       }
     }
-    return dest;
-  };  
-  
-  // the main http request function
-  var relax = function(opts, callback) {
-     if (typeof opts === 'function') {
-       callback = opts;
-       opts = {path: ''};
-     }
 
-     if (typeof opts === 'string') {
-       opts = {path: opts};
-     }
+    // prevent bugs where people set encoding when piping
+    if (opts.encoding !== undefined && callback) {
+      req.encoding = opts.encoding;
+      delete req.headers['content-type'];
+      delete req.headers.accept;
+    }
 
-     if (!opts) {
-       opts = {path: ''};
-       callback = null;
-     }
+    if (opts.contentType) {
+      req.headers['content-type'] = opts.contentType;
+      delete req.headers.accept;
+    }
 
-     var qs = munge({}, opts.qs);
+    // http://guide.couchdb.org/draft/security.html#cookies
+    if (cfg.cookie) {
+      req.headers['X-CouchDB-WWW-Authenticate'] = 'Cookie';
+      req.headers.cookie = cfg.cookie;
+    }
 
-     var headers = {
-       'content-type': 'application/json',
-       accept: 'application/json'
-     };
+    // http://wiki.apache.org/couchdb/HTTP_view_API#Querying_Options
+    if (typeof opts.qs === 'object' && !_.isEmpty(opts.qs)) {
+      ['startkey', 'endkey', 'key', 'keys'].forEach(function(key) {
+        if (key in opts.qs) {
+          qs[key] = JSON.stringify(opts.qs[key]);
+        }
+      });
+      req.qs = qs;
+    }
 
-     var req = {
-       method: (opts.method || 'GET'),
-       headers: headers,
-       uri: cfg.url
-     };
+    if (opts.body) {
+      if (Buffer.isBuffer(opts.body) || opts.dontStringify) {
+        req.body = opts.body;
+      } else {
+        req.body = JSON.stringify(opts.body, function(key, value) {
+          // don't encode functions
+          if (typeof(value) === 'function') {
+            return value.toString();
+          } else {
+            return value;
+          }
+        });
+      }
+    }
 
-     var parsed;
-     var rh;
+    if (opts.form) {
+      req.headers['content-type'] =
+        'application/x-www-form-urlencoded; charset=utf-8';
+      req.body = querystring.stringify(opts.form).toString('utf8');
+    }
 
-     // https://github.com/mikeal/request#requestjar
-     var isJar = opts.jar || cfg.jar;
+    log(req);
 
-     if (isJar) {
-       req.jar = isJar;
-     }
+    if (!callback) {
+      return httpAgent(req);
+    }
 
-     // http://wiki.apache.org/couchdb/HTTP_database_API#Naming_and_Addressing
-     if (opts.db) {
-       req.uri = urlResolveFix(req.uri, encodeURIComponent(opts.db));
-     }
+    return httpAgent(req, function(e, h, b) {
+      rh = h && h.headers || {};
+      rh.statusCode = h && h.statusCode || 500;
+      rh.uri = req.uri;
 
-     if (opts.multipart) {
-       req.multipart = opts.multipart;
-     }
+      if (e) {
+        log({err: 'socket', body: b, headers: rh});
+        return callback(errs.merge(e, {
+          message: 'error happened in your connection',
+          scope: 'socket',
+          errid: 'request'
+        }));
+      }
 
-     req.headers = munge(req.headers,opts.headers);
-     req.headers = munge(req.headers,cfg.defaultHeaders);
+      delete rh.server;
+      delete rh['content-length'];
 
-     if (opts.path) {
-       req.uri += '/' + opts.path;
-     } else if (opts.doc) {
-       if (!/^_design/.test(opts.doc)) {
-         // http://wiki.apache.org/couchdb/HTTP_Document_API#Naming.2FAddressing
-         req.uri += '/' + encodeURIComponent(opts.doc);
-       } else {
-         // http://wiki.apache.org/couchdb/HTTP_Document_API#Document_IDs
-         req.uri += '/' + opts.doc;
-       }
+      if (opts.dontParse) {
+        parsed = b;
+      } else {
+        try { parsed = JSON.parse(b); } catch (err) { parsed = b; }
+      }
 
-       // http://wiki.apache.org/couchdb/HTTP_Document_API#Attachments
-       if (opts.att) {
-         req.uri += '/' + opts.att;
-       }
-     }
+      if (rh.statusCode >= 200 && rh.statusCode < 400) {
+        log({err: null, body: parsed, headers: rh});
+        return callback(null, parsed, rh);
+      }
 
-     // prevent bugs where people set encoding when piping
-     if (opts.encoding !== undefined && callback) {
-       req.encoding = opts.encoding;
-       delete req.headers['content-type'];
-       delete req.headers.accept;
-     }
+      log({err: 'couch', body: parsed, headers: rh});
 
-     if (opts.contentType) {
-       req.headers['content-type'] = opts.contentType;
-       delete req.headers.accept;
-     }
+      // cloudant stacktrace
+      if (typeof parsed === 'string') {
+        parsed = {message: parsed};
+      }
 
-     // http://guide.couchdb.org/draft/security.html#cookies
-     if (cfg.cookie) {
-       req.headers['X-CouchDB-WWW-Authenticate'] = 'Cookie';
-       req.headers.cookie = cfg.cookie;
-     }
+      if (!parsed.message && (parsed.reason || parsed.error)) {
+        parsed.message = (parsed.reason || parsed.error);
+      }
 
-     // http://wiki.apache.org/couchdb/HTTP_view_API#Querying_Options
-     if (typeof opts.qs === 'object' && Object.keys(opts.qs).length > 0) {
-       ['startkey', 'endkey', 'key', 'keys'].forEach(function(key) {
-         if (key in opts.qs) {
-           qs[key] = JSON.stringify(opts.qs[key]);
-         }
-       });
-       req.qs = qs;
-     }
+      // fix cloudant issues where they give an erlang stacktrace as js
+      delete parsed.stack;
 
-     if (opts.body) {
-       if (Buffer.isBuffer(opts.body) || opts.dontStringify) {
-         req.body = opts.body;
-       } else {
-         req.body = JSON.stringify(opts.body, function(key, value) {
-           // don't encode functions
-           if (typeof(value) === 'function') {
-             return value.toString();
-           } else {
-             return value;
-           }
-         });
-       }
-     }
+      // scrub credentials
+      req.uri = scrub(req.uri);
+      rh.uri = scrub(rh.uri);
+      if (req.headers.cookie) {
+        req.headers.cookie = "XXXXXXX";
+      }
 
-     if (opts.form) {
-       req.headers['content-type'] =
-         'application/x-www-form-urlencoded; charset=utf-8';
-       req.body = querystring.stringify(opts.form).toString('utf8');
-     }
-
-     debug(req);
-
-     if (!callback) {
-       return httpAgent(req);
-     }
-
-     return httpAgent(req, function(e, h, b) {
-       rh = h && h.headers || {};
-       rh.statusCode = h && h.statusCode || 500;
-       rh.uri = req.uri;
-
-       if (e) {
-         debug({err: 'socket', body: b, headers: rh});
-         return callback(errs.merge(e, {
-           message: 'error happened in your connection',
-           scope: 'socket',
-           errid: 'request'
-         }));
-       }
-
-       delete rh.server;
-       delete rh['content-length'];
-
-       if (opts.dontParse) {
-         parsed = b;
-       } else {
-         try { parsed = JSON.parse(b); } catch (err) { parsed = b; }
-       }
-
-       if (rh.statusCode >= 200 && rh.statusCode < 400) {
-         debug({err: null, body: parsed, headers: rh});
-         return callback(null, parsed, rh);
-       }
-
-       debug({err: 'couch', body: parsed, headers: rh});
-
-       // cloudant stacktrace
-       if (typeof parsed === 'string') {
-         parsed = {message: parsed};
-       }
-
-       if (!parsed.message && (parsed.reason || parsed.error)) {
-         parsed.message = (parsed.reason || parsed.error);
-       }
-
-       // fix cloudant issues where they give an erlang stacktrace as js
-       delete parsed.stack;
-
-       callback(errs.merge({
-         message: 'couch returned ' + rh.statusCode,
-         scope: 'couch',
-         statusCode: rh.statusCode,
-         request: req,
-         headers: rh,
-         errid: 'non_200'
-       }, errs.create(parsed)));
-     });
-   }
+      callback(errs.merge({
+        message: 'couch returned ' + rh.statusCode,
+        scope: 'couch',
+        statusCode: rh.statusCode,
+        request: req,
+        headers: rh,
+        errid: 'non_200'
+      }, errs.create(parsed)));
+    });
+  }
  
   // get helper
   var get = function(opts, cb) {
